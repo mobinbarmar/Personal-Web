5b202a2938b899d78f1c93b34dde3e0e0f10b439
Mobin Barmar <barmar.mobin@gmail.com>
2023-04-11 15:55:44 +0430

    services, guards, pages, shared modules added

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { ThemeComponent } from './theme/theme.component';
+import { SharedModule } from './shared/shared.module';
+import { PagesModule } from './pages/pages.module';
+import { GuardsModule } from './guards/guards.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +18,10 @@ import { ThemeComponent } from './theme/theme.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    SharedModule,
+    PagesModule,
+    GuardsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
