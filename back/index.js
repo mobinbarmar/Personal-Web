07f42356b5a1f90e7094dcc45c2c39981bb738b3
Mobin Barmar <barmar.mobin@gmail.com>
2023-04-11 11:33:26 +0430

    init

---
+++ back/index.js
@@ -0,0 +1,51 @@
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+const cors = require('cors');
+
+// let indexRouter = require('./routes/index');
+// let usersRouter = require('./routes/users');
+// let providerRouter = require('./routes/providers');
+// let apiRouter = require('./api/routes/main.routes');
+
+
+let app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors({'origin': 'http://localhost:4200'}))
+
+app.get('/', (req,res) => {
+    res.send('mobin')
+})
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+// app.use('/providers', providerRouter)
+// app.use('/api', apiRouter)
+// app.use('/*', indexRouter)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(3000, () => {
+    console.log('http://localhost:3000');
+})
