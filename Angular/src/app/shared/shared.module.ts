31dffabc1f3aab8e3264a2d0f793c18cf5e1f561
Mobin Barmar <barmar.mobin@gmail.com>
2023-05-02 11:16:12 +0330

    logo-component is added

--- Angular/src/app/shared/shared.module.ts
+++ Angular/src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Card1Component } from './card1/card1.component';
+import { LogoComponent } from './logo/logo.component';
 
 
 
 @NgModule({
   declarations: [
-    Card1Component
+    Card1Component,
+    LogoComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
-    Card1Component
+    Card1Component,
+    LogoComponent
   ]
 })
 export class SharedModule { }
