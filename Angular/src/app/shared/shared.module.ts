c8f3811c80a669345427572fbb7b99d3f438978e
Mobin Barmar <barmar.mobin@gmail.com>
2023-05-02 10:56:34 +0330

    doing card1

--- Angular/src/app/shared/shared.module.ts
+++ Angular/src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Card1Component } from './card1/card1.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    Card1Component
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    Card1Component
   ]
 })
 export class SharedModule { }
