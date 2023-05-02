c2e4920dfbc64f86973e06e4ac3cfeb0d501fbef
Mobin Barmar <barmar.mobin@gmail.com>
2023-05-02 14:13:45 +0330

    logo Input() width is added

--- Angular/src/app/shared/logo/logo.component.ts
+++ Angular/src/app/shared/logo/logo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-logo',
@@ -11,5 +11,7 @@ export class LogoComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  @Input() width:string = '60px'
 
 }
