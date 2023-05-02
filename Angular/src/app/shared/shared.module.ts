224cd93abb48c9443a257a613ec9bb205de80045
Mobin Barmar <barmar.mobin@gmail.com>
2023-05-02 11:22:18 +0330

    home-component, nav-component are added

--- Angular/src/app/shared/shared.module.ts
+++ Angular/src/app/shared/shared.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Card1Component } from './card1/card1.component';
 import { LogoComponent } from './logo/logo.component';
+import { NavComponent } from './nav/nav.component';
 
 
 
 @NgModule({
   declarations: [
     Card1Component,
-    LogoComponent
+    LogoComponent,
+    NavComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
     Card1Component,
-    LogoComponent
+    LogoComponent,
+    NavComponent
   ]
 })
 export class SharedModule { }
